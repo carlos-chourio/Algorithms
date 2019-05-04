4970f944d419f7f3d655c5840e902673c912992d
carlos-chourio <carlos18daniel@gmail.com>
2019-05-04 10:45:01 -0400

    Creates Algorithm Solution. Adds Sieve prime numbers algorithm

---
+++ Algorithms/Mathematics/NumberTheory/Primes.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithms.Mathematics.NumberTheory {
+    public class Primes {
+        public IEnumerable<int> GetPrimesSieve(int finalNumber) {
+            List<int> numbers = new List<int>();
+            numbers.AddRange(Enumerable.Range(2, finalNumber-1));
+            int index = 1;
+            int currentValue = numbers[0]*2;
+            int pivot = numbers[0];
+            int fullTurns = 0;
+            while (true) {
+                if (currentValue == numbers[index]) {
+                    numbers.RemoveAt(index);
+                } else {
+                    if (numbers[index] > currentValue) {
+                        currentValue += pivot;
+                    }
+                    index++;
+                }
+                if (index == numbers.Count) {
+                    if (numbers[fullTurns] > numbers[numbers.Count - 1]/2) {
+                        break;
+                    }
+                    fullTurns++;
+                    index = fullTurns;
+                    currentValue = numbers[fullTurns]*2;
+                    pivot = numbers[fullTurns];
+                }
+            }
+            return numbers;
+        }
+    }
+}
---
+++ Algorithms/Program.cs
@@ -0,0 +1,19 @@
+﻿using Algorithms.Mathematics.NumberTheory;
+using System;
+using System.Collections.Generic;
+
+namespace Algorithms {
+    class Program {
+        static void Main(string[] args) {
+            Primes primeGenerator = new Primes();
+            int finalNumber = 100;
+            var primes = primeGenerator.GetPrimesSieve(finalNumber);
+            int i = 0;
+            Console.WriteLine($"There are {((IList<int>)primes).Count} primes from 2 to {finalNumber}");
+            foreach (var prime in primes) {
+                Console.WriteLine($"prime {++i}: {prime}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
